40c42228b27c05452c45577f93bf1b40c0949ee4
AbdelRaouf Dannaoui <123raoufdannaoui@gmail.com>
2021-01-23 11:42:23 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,46 @@
 // Code YouTubeDebugger Component Here
+import React from "react";
+
+class YouTubeDebugger extends React.Component{
+  constructor(){
+    super();
+
+    this.state = {
+          errors: [],
+          user: null,
+          settings: {
+              bitrate: 8,
+              video: {
+                resolution: '1080p'
+              }
+          }
+    }
+  }
+
+  changer1 = () =>{
+    this.setState(
+      {
+        settings: {
+          ...this.state.settings, bitrate: 12
+        }
+      });
+  };
+
+  changer2 = () =>{
+    this.setState(
+      {
+        settings: {
+          ...this.state.settings, video: {
+            ...this.state.settings.video, resolution: "720p"
+          }
+        }
+      });
+  };
+
+  render(){
+    return (
+      <button onClick={this.changer1} > this.state.settings.bitrate</button>
+      <button onClick={this.changer2} > this.state.settings.video.resolution</button>
+    )
+  }
+}
